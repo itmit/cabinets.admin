dab2d45da7e445b11d7e12c02bf87675c9537403
Bronyrus <bronyruschannel@gmail.com>
2020-01-23 14:03:03 +0300

    Update cabinetsEdit.blade.php

--- resources/views/cabinets/cabinetsEdit.blade.php
+++ resources/views/cabinets/cabinetsEdit.blade.php
@@ -28,6 +28,78 @@
                     </div>
                 </div>
 
+                <div class="form-group{{ $errors->has('area') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="area" class="control-label text-tc">Площадь</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <input id="area" type="number" step="0.1" min="1" max="9999" class="form-control" name="area" value="{{ $cabinet->area }}" required maxlength="191">
+            
+                        @if ($errors->has('area'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('area') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+            
+                <div class="row form-group{{ $errors->has('capacity') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="capacity" class="control-label text-tc">Вместимость</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <input id="capacity" type="number" step="1" min="1" max="999" class="form-control" name="capacity" value="{{ $cabinet->capacity }}" required maxlength="191">
+            
+                        @if ($errors->has('capacity'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('capacity') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="row form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="description" class="control-label text-tc">Описание</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <textarea name="description" cols="30" rows="10" class="form-control">{{ $cabinet->description }}</textarea>
+            
+                        @if ($errors->has('description'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('description') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('photos') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="photos" class=" control-label text-tc">Фотографии</label>
+                    </div>
+                
+                    <div class="col-xs-12 col-sm-10">
+                        <input type="file" name="photos[]" id="photos" class="form-control-file" accept="image/*" multiple>
+            
+                        @if ($errors->has('photos'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('photos') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+            
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-tc-ct">
+                            Сохранить
+                        </button>
+                    </div>
+                </div>
+
             </form>
 
             <div class="row">
