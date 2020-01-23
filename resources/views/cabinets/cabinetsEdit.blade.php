a1182eb45c735c4618c54f57fb9149cbbdd4a482
Bronyrus <bronyruschannel@gmail.com>
2020-01-23 15:56:03 +0300

    Update cabinetsEdit.blade.php

--- resources/views/cabinets/cabinetsEdit.blade.php
+++ resources/views/cabinets/cabinetsEdit.blade.php
@@ -9,7 +9,7 @@
 
             <a href="../cabinets">Назад</a>
 
-            <form class="form-horizontal" method="PUT" action="{{ route('auth.cabinets.update', ['id' => $cabinet->id]),  }}" enctype="multipart/form-data">
+            <form class="form-horizontal" method="PATCH" action="{{ route('auth.cabinets.update', ['id' => $cabinet->id]),  }}" enctype="multipart/form-data">
                 {{ csrf_field() }}
             
                 <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
