c11a1fe3a6290ff965caecbc76cca9b6c62e5f2b
Bronyrus <bronyruschannel@gmail.com>
2020-01-23 13:54:49 +0300

    Update cabinetsEdit.blade.php

--- resources/views/cabinets/cabinetsEdit.blade.php
+++ resources/views/cabinets/cabinetsEdit.blade.php
@@ -9,6 +9,27 @@
 
             <a href="../cabinets">Назад</a>
 
+            <form class="form-horizontal" method="POST" action="{{ route('auth.cabinets.edit') }}" enctype="multipart/form-data">
+                {{ csrf_field() }}
+            
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="name" class="control-label text-tc">Название кабинета</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <input id="name" type="text" class="form-control" name="name" value="{{ $cabinet->name }}" required maxlength="191" placeholder=" Кабинет #">
+            
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+            </form>
+
             <div class="row">
                 @foreach($photos as $item)
 
