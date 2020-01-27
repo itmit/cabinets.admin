4c5bfda6921793f3df34634ab04ed55b8bc919e0
Bronyrus <bronyruschannel@gmail.com>
2020-01-27 14:13:47 +0300

    Update CabinetReservationApiController.php

--- app/Http/Controllers/Api/CabinetReservationApiController.php
+++ app/Http/Controllers/Api/CabinetReservationApiController.php
@@ -50,7 +50,7 @@ class CabinetReservationApiController extends ApiBaseController
 
         // return json_encode( $resTime );
 
-        $freeTimes = array_diff($times, $reservation);
+        $freeTimes = array_diff($times[0], $reservation[0]);
 
         // foreach($times as $item => $key)
         // {
