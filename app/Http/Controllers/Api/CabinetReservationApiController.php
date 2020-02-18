ac275b15dc132d1d1c911beff753c215931c3c34
Bronyrus <bronyruschannel@gmail.com>
2020-02-18 14:23:04 +0300

    Update CabinetReservationApiController.php

--- app/Http/Controllers/Api/CabinetReservationApiController.php
+++ app/Http/Controllers/Api/CabinetReservationApiController.php
@@ -335,7 +335,7 @@ class CabinetReservationApiController extends ApiBaseController
             ];
             $times = CabinetReservationTime::where('reservation_id', $reservation->id)->get();
             foreach ($times as $time) {
-                $result[][] = ['times' => $time->time];
+                $result[]['times'] = ['time' => $time->time];
             }
         }
 
