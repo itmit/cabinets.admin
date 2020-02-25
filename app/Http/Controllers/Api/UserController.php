e8bb739cbdbc2bb68f3673ac5cf29fc5ef3548a8
Bronyrus <bronyruschannel@gmail.com>
2020-02-25 16:12:50 +0300

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Client;
+use App\Models\CabinetReservation;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
