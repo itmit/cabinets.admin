7310825f5be38681f9e481ddfd8b55f2a4818c54
Bronyrus <bronyruschannel@gmail.com>
2020-02-25 11:51:51 +0300

    Update clientDetail.blade.php

--- resources/views/clients/clientDetail.blade.php
+++ resources/views/clients/clientDetail.blade.php
@@ -9,21 +9,15 @@
 
             <a href="../clients">Назад</a>
             
-            {{-- <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+            <div class="form-group">
                 <div class="col-xs-12 col-sm-2">
-                <label for="name" class="control-label text-tc">Название кабинета</label>
+                <label for="name" class="control-label text-tc">Телефон</label>
                 </div>
         
                 <div class="col-xs-12 col-sm-10">
-                    <input id="name" type="text" class="form-control" name="name" value="{{ $cabinet->name }}" required maxlength="191" placeholder=" Кабинет #">
-        
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
-                        </span>
-                    @endif
+                    <p>{{ $client->phone }}</p>
                 </div>
-            </div> --}}
+            </div>
             
         </div>
     </div>
