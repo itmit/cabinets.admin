3a983d3b50c752ad8923466f5664703eb29ea3e1
Bronyrus <bronyruschannel@gmail.com>
2020-02-25 12:12:07 +0300

    Update clientDetail.blade.php

--- resources/views/clients/clientDetail.blade.php
+++ resources/views/clients/clientDetail.blade.php
@@ -9,56 +9,31 @@
 
             <a href="../clients">Назад</a>
             
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-2">
-                <label for="phone" class="control-label text-tc">Телефон</label>
-                </div>
-        
-                <div class="col-xs-12 col-sm-10">
-                    <p id="phone">{{ $client->phone }}</p>
-                </div>
+            <div class="row">
+                <div class="col-md-6">Телефон</div>
+                <div class="col-md-6"><p>{{ $client->phone }}</p></div>
             </div>
 
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-2">
-                <label for="email" class="control-label text-tc">Эл. почта</label>
-                </div>
-        
-                <div class="col-xs-12 col-sm-10">
-                    <p id="email">{{ $client->email }}</p>
-                </div>
+            <div class="row">
+                <div class="col-md-6">Эл. почта</div>
+                <div class="col-md-6"><p>{{ $client->email }}</p></div>
             </div>
 
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-2">
-                <label for="birthday" class="control-label text-tc">Дата рождения</label>
-                </div>
-        
-                <div class="col-xs-12 col-sm-10">
-                    <p id="birthday">{{ $client->birthday }}</p>
-                </div>
+            <div class="row">
+                <div class="col-md-6">Дата рождения</div>
+                <div class="col-md-6"><p>{{ $client->birthday }}</p></div>
             </div>
 
             <hr>
 
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-2">
-                <label for="getReservationCount" class="control-label text-tc">Количество бронирований</label>
-                </div>
-        
-                <div class="col-xs-12 col-sm-10">
-                    <p id="getReservationCount">{{ $client->getReservationCount() }}</p>
-                </div>
+            <div class="row">
+                <div class="col-md-6">Количество бронирований</div>
+                <div class="col-md-6"><p>{{ $client->getReservationCount() }}</p></div>
             </div>
 
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-2">
-                <label for="amount" class="control-label text-tc">Общая сумма к оплате</label>
-                </div>
-        
-                <div class="col-xs-12 col-sm-10">
-                    <p id="amount">{{ $amount }} рублей</p>
-                </div>
+            <div class="row">
+                <div class="col-md-6">Общая сумма к оплате</div>
+                <div class="col-md-6"><p>{{ $amount }}</p></div>
             </div>
             
         </div>
