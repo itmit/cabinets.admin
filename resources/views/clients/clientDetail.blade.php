fe3bcd6b4e1e7c8799e92f2b1cc5091d3f8710a0
Bronyrus <bronyruschannel@gmail.com>
2020-02-25 12:42:42 +0300

    Update clientDetail.blade.php

--- resources/views/clients/clientDetail.blade.php
+++ resources/views/clients/clientDetail.blade.php
@@ -10,30 +10,30 @@
             <a href="../clients">Назад</a>
             
             <div class="row">
-                <div class="col-md-6">Телефон</div>
-                <div class="col-md-6"><p>{{ $client->phone }}</p></div>
+                <div class="col-md-4">Телефон</div>
+                <div class="col-md-8"><p>{{ $client->phone }}</p></div>
             </div>
 
             <div class="row">
-                <div class="col-md-6">Эл. почта</div>
-                <div class="col-md-6"><p>{{ $client->email }}</p></div>
+                <div class="col-md-4">Эл. почта</div>
+                <div class="col-md-8"><p>{{ $client->email }}</p></div>
             </div>
 
             <div class="row">
-                <div class="col-md-6">Дата рождения</div>
-                <div class="col-md-6"><p>{{ $client->birthday }}</p></div>
+                <div class="col-md-4">Дата рождения</div>
+                <div class="col-md-8"><p>{{ $client->birthday }}</p></div>
             </div>
 
             <hr>
 
             <div class="row">
-                <div class="col-md-6">Количество бронирований</div>
-                <div class="col-md-6"><p>{{ $client->getReservationCount() }}</p></div>
+                <div class="col-md-4">Количество бронирований</div>
+                <div class="col-md-8"><p>{{ $client->getReservationCount() }}</p></div>
             </div>
 
             <div class="row">
-                <div class="col-md-6">Общая сумма к оплате</div>
-                <div class="col-md-6"><p>{{ $amount }}</p></div>
+                <div class="col-md-4">Общая сумма к оплате</div>
+                <div class="col-md-8"><p>{{ $amount }}</p></div>
             </div>
 
             @foreach ($reservations as $item)
@@ -45,12 +45,12 @@
 
                 <div class="row">
                     <div class="col-sm-9">
-                      <p>Бронирование кабинета {{ $cabinet->name }}<i class="material-icons" style="color: {{ $cabinet->color }}">home</i> ({{ $item->date }})</p>
+                        <p>Бронирование кабинета <b><a href="../cabinets/{{ $cabinet->id }}">{{ $cabinet->name }}</a></b><i class="material-icons" style="color: {{ $cabinet->color }}">home</i> ({{ $item->date }})</p>
                       <div class="row">
-                        <div class="col-xs-8 col-sm-6">
+                        <div class="col-xs-4 col-sm-4">
                             <p>Время: </p>
                         </div>
-                        <div class="col-xs-4 col-sm-6">
+                        <div class="col-xs-8 col-sm-8">
                             <ul>
                                 @foreach ($times as $time)
                                 <li>
@@ -59,10 +59,10 @@
                                 @endforeach
                             </ul>
                         </div>
-                        <div class="col-xs-8 col-sm-6">
+                        <div class="col-xs-4 col-sm-8">
                             <p>Стоимость: </p>
                         </div>
-                        <div class="col-xs-4 col-sm-6">
+                        <div class="col-xs-4 col-sm-8">
                             <p>
                                 {{ $item->total_amount }} рублей
                             </p>    
