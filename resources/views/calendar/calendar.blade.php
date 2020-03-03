2be5e2a115c7876cd20b7a6a8dfdf478f1a49649
Bronyrus <bronyruschannel@gmail.com>
2020-03-03 10:40:16 +0300

    Update calendar.blade.php

--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -180,16 +180,22 @@
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
             // The type of chart we want to create
-            type: 'line',
+            type: 'bar',
 
             // The data for our dataset
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['26.03.2020'],
                 datasets: [{
-                    label: 'My First dataset',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
+                    label: 'Кабинет №1',
+                    backgroundColor: '#ff8080',
+                    borderColor: '#ff8080',
+                    data: [20, 30, 45]
+                },
+                {
+                    label: 'Кабинет №2',
+                    backgroundColor: '#0000ff',
+                    borderColor: '#0000ff',
+                    data: [0, 10, 5, 2]
                 }]
             },
 
