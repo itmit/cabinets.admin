13e081bbd494c512379e3690582c92cc1650ef22
Bronyrus <bronyruschannel@gmail.com>
2020-03-04 12:36:48 +0300

    Update CabinetReservationApiController.php

--- app/Http/Controllers/Api/CabinetReservationApiController.php
+++ app/Http/Controllers/Api/CabinetReservationApiController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Spatie\GoogleCalendar\Event;
 
 class CabinetReservationApiController extends ApiBaseController
 {
@@ -186,6 +187,22 @@ class CabinetReservationApiController extends ApiBaseController
                             'time' => $value,
                             'price' => intdiv($price, 2)
                         ]);
+
+                        $event = new Event;
+
+                        $startEndTime = explode('-', $value);
+
+                        $startDateTime = '' . $request->date . 'T' . $startEndTime[0] . ':00.0z';
+                        $endDateTime = '' . $request->date . 'T' . $startEndTime[1] . ':00.0z';
+
+                        // $format = 'Y-m-d';
+                        // $date = DateTime::createFromFormat($format, '2009-02-15');
+
+                        $event->name = 'Кабинет' . $cabinet->id;
+                        $event->startDateTime = $startDateTime;
+                        $event->endDateTime = $endDateTime;
+
+                        $event->save();
 
                         CabinetReservation::where('id', $resId)->update([
                             'total_amount' => $amount
@@ -377,40 +394,40 @@ class CabinetReservationApiController extends ApiBaseController
     private function workingTime()
     {
         // первая стоимость
-        $time[0] = '7.00-7.30'; 
-        $time[1] = '7.30-8.00';
-        $time[2] = '8.00-8.30';
-        $time[3] = '8.30-9.00';
-        $time[4] = '9.00-9.30';
-        $time[5] = '9.30-10.00';
-        $time[6] = '10.00-10.30';
-        $time[7] = '10.30-11.00';
-        $time[8] = '11.00-11.30';
-        $time[9] = '11.30-12.00';
-        $time[10] = '12.00-12.30';
-        $time[11] = '12.30-13.00';
-        $time[12] = '13.00-13.30';
-        $time[13] = '13.30-14.00';
-        $time[14] = '14.00-14.30';
-        $time[15] = '14.30-15.00';
-        $time[16] = '15.00-15.30';
-        $time[17] = '15.30-16.00';
-        $time[18] = '16.00-16.30';
+        $time[0] = '7:00-7:30'; 
+        $time[1] = '7:30-8:00';
+        $time[2] = '8:00-8:30';
+        $time[3] = '8:30-9:00';
+        $time[4] = '9:00-9:30';
+        $time[5] = '9:30-10:00';
+        $time[6] = '10:00-10:30';
+        $time[7] = '10:30-11:00';
+        $time[8] = '11:00-11:30';
+        $time[9] = '11:30-12:00';
+        $time[10] = '12:00-12:30';
+        $time[11] = '12:30-13:00';
+        $time[12] = '13:00-13:30';
+        $time[13] = '13:30-14:00';
+        $time[14] = '14:00-14:30';
+        $time[15] = '14:30-15:00';
+        $time[16] = '15:00-15:30';
+        $time[17] = '15:30-16:00';
+        $time[18] = '16:00-16:30';
 
         // вторая стоимость
-        $time[19] = '16.30-17.00'; 
-        $time[20] = '17.00-17.30';
-        $time[21] = '17.30-18.00';
-        $time[22] = '18.00-18.30';
-        $time[23] = '18.30-19.00';
-        $time[24] = '19.00-19.30';
-        $time[25] = '19.30-20.00';
-        $time[26] = '20.00-20.30';
-        $time[27] = '20.30-21.00';
-        $time[28] = '21.00-21.30';
-        $time[29] = '21.30-22.00';
-        $time[30] = '22.00-22.30';
-        $time[31] = '22.30-23.00';
+        $time[19] = '16:30-17:00'; 
+        $time[20] = '17:00-17:30';
+        $time[21] = '17:30-18:00';
+        $time[22] = '18:00-18:30';
+        $time[23] = '18:30-19:00';
+        $time[24] = '19:00-19:30';
+        $time[25] = '19:30-20:00';
+        $time[26] = '20:00-20:30';
+        $time[27] = '20:30-21:00';
+        $time[28] = '21:00-21:30';
+        $time[29] = '21:30-22:00';
+        $time[30] = '22:00-22:30';
+        $time[31] = '22:30-23:00';
 
         return $time;
     }
