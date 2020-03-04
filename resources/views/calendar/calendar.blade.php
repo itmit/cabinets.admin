729cd8ed9eaf9e5474b55681ddb42916c4770c93
Bronyrus <bronyruschannel@gmail.com>
2020-03-04 11:09:54 +0300

    Update calendar.blade.php

--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -27,6 +27,7 @@
                         {{-- <div class="list">
                             Для начала выберите дату!
                         </div> --}}
+                        <iframe src="https://calendar.google.com/calendar/embed?src=fondoompsy%40gmail.com&ctz=Europe%2FMoscow" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
             </div>
