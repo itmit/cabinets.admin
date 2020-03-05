2f5e216ef2a8d159647afc06e675b48f24713780
Bronyrus <bronyruschannel@gmail.com>
2020-03-05 10:30:21 +0300

    Update cabinetsEdit.blade.php

--- resources/views/cabinets/cabinetsEdit.blade.php
+++ resources/views/cabinets/cabinetsEdit.blade.php
@@ -161,7 +161,7 @@
                         headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
                         dataType: "json",
                         data    : { photo: photo },
-                        url     : '../../deletePhoto',
+                        url     : '../cabinets/deletePhoto',
                         method    : 'post',
                         success: function (response) {
                             delPhoto.remove();
