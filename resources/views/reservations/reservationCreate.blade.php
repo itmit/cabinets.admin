cdd9427d5317ba53fb898b9a7680c494d2941113
Bronyrus <bronyruschannel@gmail.com>
2020-03-09 11:24:37 +0300

    Update reservationCreate.blade.php

--- resources/views/reservations/reservationCreate.blade.php
+++ resources/views/reservations/reservationCreate.blade.php
@@ -9,27 +9,6 @@
             <form class="form-horizontal" method="POST" action="{{ route('auth.reservations.store') }}" enctype="multipart/form-data">
                 {{ csrf_field() }}
             
-                <div class="form-group{{ $errors->has('cabinet') ? ' has-error' : '' }}">
-                    <div class="col-xs-12 col-sm-2">
-                    <label for="cabinet" class="control-label text-tc">Выбрать кабинет</label>
-                    </div>
-            
-                    <div class="col-xs-12 col-sm-10">
-                        <select name="cabinet" class="form-control" required>
-                            <option selected disabled>Кабинет</option>
-                            @foreach ($cabinets as $cabinet)
-                                <option value="{{ $cabinet->id }}">{{ $cabinet->name }}</option>
-                            @endforeach
-                        </select>
-            
-                        @if ($errors->has('cabinet'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('cabinet') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
-
                 <div class="form-group{{ $errors->has('client') ? ' has-error' : '' }}">
                     <div class="col-xs-12 col-sm-2">
                     <label for="client" class="control-label text-tc">Выбрать клиента</label>
@@ -37,7 +16,7 @@
             
                     <div class="col-xs-12 col-sm-10">
                         <select name="client" class="form-control" required>
-                            <option selected disabled>Клиент</option>
+                            <option value="" selected disabled>Клиент</option>
                             @foreach ($clients as $client)
                                 <option value="{{ $client->id }}">{{ $client->name }}</option>
                             @endforeach
@@ -51,7 +30,61 @@
                     </div>
                 </div>
 
-                
+                <div class="form-group{{ $errors->has('cabinet') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="cabinet" class="control-label text-tc">Выбрать кабинет</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <select name="cabinet" class="form-control" required>
+                            <option value="" selected disabled>Кабинет</option>
+                            @foreach ($cabinets as $cabinet)
+                                <option value="{{ $cabinet->id }}">{{ $cabinet->name }}</option>
+                            @endforeach
+                        </select>
+            
+                        @if ($errors->has('cabinet'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('cabinet') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('date') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="date" class="control-label text-tc">Выбрать дату</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <input type="date" name="date" class="form-control" required>
+            
+                        @if ($errors->has('date'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('date') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('times') ? ' has-error' : '' }}">
+                    <div class="col-xs-12 col-sm-2">
+                    <label for="times" class="control-label text-tc">Выбрать время</label>
+                    </div>
+            
+                    <div class="col-xs-12 col-sm-10">
+                        <select name="times" class="form-control" required>
+                            <option value="" selected disabled>Время</option>
+                            
+                        </select>
+            
+                        @if ($errors->has('times'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('times') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
             
                 <div class="form-group">
                     <div class="col-sm-12">
